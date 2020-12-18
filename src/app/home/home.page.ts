7bd547bb325a8b0f264779e0af20102356160060
ZehCoque <jose.coque@hotmail.com>
2020-12-17 23:04:23 -0300

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  token: any = 'bd53607fa0fa5c322b93912dc556e4d4591f7d6f3219ff51714b1769e12cbbcd';
+  elementType: string = 'url';
+
+  constructor() {}
+
+  createCode() {
+
+  }
+
+}
